d509a0ce13e17dca3e5cd32e05fc758473b2b36f
Samir Baraily <35265993+sbaraily@users.noreply.github.com>
2022-12-19 01:19:46 +0000

    Initial commit

---
+++ ifelse.py
@@ -0,0 +1,8 @@
+applePrice = 210
+budget = 210
+if(applePrice < budget):
+  print("Alexa, add 1 kg Apples to the cart.")
+elif(applePrice == budget):
+  print("You're right on budget")
+else:
+  print("Alexa, do not add Apples to the cart.")
---
+++ main.py
@@ -0,0 +1,19 @@
+a = int(input("Enter your age: "))
+print("Your age is: ", a)
+#Conditional Operators are >,<,<=,>=, --
+# print(a>18)
+# print(a<18)
+# print(a>=18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+if(a>18):
+  print("You can drive")
+  print("Yes")
+else:
+  print("Your cannot drive")
+  print("No")
+
+
+
+
